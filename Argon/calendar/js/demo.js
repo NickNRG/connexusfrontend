c2565bc2ff85a7d61951987ad8a406137d715f06
NickNRG <NickNRG@users.noreply.github.com>
2018-10-14 17:11:24 +0530

    Demo.js script added to html

--- Argon/calendar/js/demo.js
+++
@@ -1,8 +0,0 @@
-var events = [
-  {'Date': new Date(2018, 6, 6), 'Title': 'Odyssey Begins!', 'Link':'https://www.odyssey2018.tk'},
-  {'Date': new Date(2018, 5, 25), 'Title': 'Unit Test Begins!', 'Link': '#'},
-  {'Date': new Date(2018, 5, 13), 'Title': 'NPS 10 year anniversary', 'Link': '#'},
-];
-var settings = {};
-var element = document.getElementById('caleandar');
-caleandar(element, events, settings);
